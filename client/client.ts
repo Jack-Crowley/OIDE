516e3c3cd32080e26be7b576b5ea0fc0978942b1
Shawn Varghese <svarghs8531@gmail.com>
2023-01-17 13:25:51 -0500

    added type annotations and all that funky jazz

--- client/client.ts
+++ client/client.ts
@@ -1,12 +1,13 @@
+import { WebSocket, Event, MessageEvent } from "ws";
 // Create WebSocket connection.
 const socket = new WebSocket('ws://localhost:8080'); 
 
 // Connection opened
-socket.addEventListener('open', function (event) {
+socket.addEventListener('open', function (event: Event) {
     socket.send('Hello Server!');
 });
 
 // Listen for messages
-socket.addEventListener('message', function (event) {
+socket.addEventListener('message', function (event: MessageEvent) {
     console.log('Message from server ', event.data);
 });
--- server/connection.ts
+++ server/connection.ts
@@ -1,12 +1,12 @@
-const WebSocket = require('ws');
+import {RawData, Server, WebSocket} from 'ws';
 // Set up server
-const wss = new WebSocket.Server({ port: 8080 });
+const wss: Server = new WebSocket.Server({ port: 8080 });
 
 // Wire up some logic for the connection event (when a client connects) 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws: WebSocket) {
 
     // Wire up logic for the message event (when a client sends something)
-    ws.on('message', function incoming(message) {
+    ws.on('message', function incoming(message: RawData) {
     console.log('received: %s', message);
     });
 
