e06101eeab8f72611ff560e8d7b4ffeed7c7b5d1
Shawn Varghese <svarghs8531@gmail.com>
2023-01-17 20:32:02 -0500

    added python running foundation and updated client

--- client/client.ts
+++ client/client.ts
@@ -1,13 +1,25 @@
 import { WebSocket, Event, MessageEvent } from "ws";
 // Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8080'); 
+const socket = new WebSocket('ws://150.136.218.146:8080'); 
+
+let interval: NodeJS.Timer;
+
+let i = 0;
+function sendMessage() {
+    socket.send(`Hello Server! #${i}`);
+    if (++i == 10) {
+        clearInterval(interval);
+        socket.close();
+    }
+}
 
 // Connection opened
-socket.addEventListener('open', function (event: Event) {
-    socket.send('Hello Server!');
+socket.addEventListener('open', function (event: Event) { 
+    interval = setInterval(sendMessage, 100);
 });
 
 // Listen for messages
 socket.addEventListener('message', function (event: MessageEvent) {
     console.log('Message from server ', event.data);
+    
 });
