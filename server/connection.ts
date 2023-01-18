32e379661fa4f8a9e680013e3489dcc6d3f3e828
Shawn Varghese <svarghs8531@gmail.com>
2023-01-17 19:32:32 -0500

    yippee!

--- server/connection.ts
+++ server/connection.ts
@@ -1,6 +1,6 @@
 import {RawData, Server, WebSocket} from 'ws';
 // Set up server
-const wss: Server = new WebSocket.Server({ port: 8080 });
+const wss: Server = new Server({ port: 8080 });
 
 // Wire up some logic for the connection event (when a client connects) 
 wss.on('connection', function connection(ws: WebSocket) {
