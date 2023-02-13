ee34a9f0b26f17a3d2a9a9b323b1133070c71d81
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-02-13 15:50:09 -0500

    repos

--- db/test.js
+++ db/test.js
@@ -1,10 +1,10 @@
 const db = require("./connect");
 const fs = require("fs")
 
-const getAccountName=fs.readFileSync(__dirname + "\\queries\\selectAccountID.sql", { encoding: "UTF-8" })
-const read_stuff_table_sql = `select * from account where email="a"`;
+const getAccountName=fs.readFileSync(__dirname + "\\queries\\insertRepo.sql", { encoding: "UTF-8" })
+const read_stuff_table_sql = `select * from repo where email="a"`;
 
-db.execute(getAccountName, ["test@gmail.com"], 
+db.execute(getAccountName, ["TestRepo",2], 
     (error, results) => {
         if (error) 
             throw error;
